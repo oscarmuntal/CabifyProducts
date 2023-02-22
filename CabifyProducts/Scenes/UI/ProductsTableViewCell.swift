0128b488f33d796dda0e1255812c70124387c716
Òscar Muntal <oscar.muntal@gmail.com>
2023-02-22 20:11:05 +0100

    [Products]: tableView and custom cell with dummy data implemented

---
+++ CabifyProducts/Scenes/UI/ProductsTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ProductsTableViewCell.swift
+//  CabifyProducts
+//
+//  Created by Òscar Muntal on 22/2/23.
+//
+
+import UIKit
+
+class ProductsTableViewCell: UITableViewCell {
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var promotion: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CabifyProducts/Scenes/UI/ProductsView.swift
+++ CabifyProducts/Scenes/UI/ProductsView.swift
@@ -8,11 +8,29 @@
 import UIKit
 
 class ProductsView: UIViewController, CreatableView, ProductsViewContract {
+    @IBOutlet weak var tableView: UITableView!
     var presenter: ProductsPresenterContract?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.view = self
+    }
+    
+}
+
+extension ProductsView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "productCell", for: indexPath) as? ProductsTableViewCell else { fatalError() }
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120.0
     }
     
 }
