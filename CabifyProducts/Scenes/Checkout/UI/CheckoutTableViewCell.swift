ec13b4cb2b14eb729b73af16337f266653a7bc91
Òscar Muntal <oscar.muntal@gmail.com>
2023-02-26 23:11:43 +0100

    [Checkout]: TableView with dummy empty cell

---
+++ CabifyProducts/Scenes/Checkout/UI/CheckoutTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CheckoutTableViewCell.swift
+//  CabifyProducts
+//
+//  Created by Òscar Muntal on 26/2/23.
+//
+
+import UIKit
+
+class CheckoutTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CabifyProducts/Scenes/Checkout/UI/CheckoutView.swift
+++ CabifyProducts/Scenes/Checkout/UI/CheckoutView.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class CheckoutView: UIViewController, CreatableView, CheckoutViewContract {
+class CheckoutView: UIViewController, CreatableView, ViewWithTable, CheckoutViewContract {
 
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var subtotal: UILabel!
     @IBOutlet weak var shipping: UILabel!
     @IBOutlet weak var total: UILabel!
@@ -17,16 +18,38 @@ class CheckoutView: UIViewController, CreatableView, CheckoutViewContract {
         print("tá tó pagao!")
     }
     var presenter: CheckoutPresenterContract?
+    var table: UITableView { tableView }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.view = self
-        title = "Checkout"
-        let totalPrice: Double = presenter?.totalPrice ?? 0.0
-        subtotal.text = "\(totalPrice) €"
-        shipping.text = "Free"
-        total.text = "\(totalPrice) €"
-        payButton.setTitle("Pay \(totalPrice) €", for: .normal)
+        setup()
     }
 
+}
+
+extension CheckoutView {
+    func setup() {
+        let totalPrice: Double = presenter?.totalPrice ?? 0.0
+        DispatchQueue.main.async {
+            self.title = "Checkout"
+            self.subtotal.text = "\(totalPrice) €"
+            self.shipping.text = "Free"
+            self.total.text = "\(totalPrice) €"
+            self.payButton.setTitle("Pay \(totalPrice) €", for: .normal)
+        }
+    }
+}
+
+extension CheckoutView: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "checkoutCell", for: indexPath) as? CheckoutTableViewCell else { fatalError() }
+        return cell
+    }
+    
+    
 }
